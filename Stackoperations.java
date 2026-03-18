2310acf5bfffb133b88bffb3a7ba3f92f196c68c
陳羽偵 <d13405011@ems.npu.edu.tw>
2026-03-18 16:29:03 +0800

    Update Stackoperations.java

--- Stackoperations.java
+++ Stackoperations.java
@@ -1,63 +1,25 @@
-class Stack {
-    private int maxSize;
-    private int[] stackArray;
-    private int top;
+import java.util.Stack;
 
-    // 建構子
-    public Stack(int size) {
-        maxSize = size;
-        stackArray = new int[maxSize];
-        top = -1;
-    }
-
-    // push：放入元素
-    public void push(int value) {
-        if (top == maxSize - 1) {
-            System.out.println("Stack Overflow");
-        } else {
-            stackArray[++top] = value;
-            System.out.println("Push: " + value);
-        }
-    }
-
-    // pop：取出元素
-    public int pop() {
-        if (isEmpty()) {
-            System.out.println("Stack Underflow");
-            return -1;
-        } else {
-            return stackArray[top--];
-        }
-    }
-
-    // peek：查看最上面
-    public int peek() {
-        if (isEmpty()) {
-            System.out.println("Stack is empty");
-            return -1;
-        } else {
-            return stackArray[top];
-        }
-    }
-
-    // 判斷是否為空
-    public boolean isEmpty() {
-        return (top == -1);
-    }
-}
-public class Stackoperations {
+public class StackExample {
     public static void main(String[] args) {
-        Stack s = new Stack(5);
+        long startTime = System.nanoTime();
 
-        s.push(10);
-        s.push(20);
-        s.push(30);
+        Stack<Integer> stack = new Stack<>();
 
-        System.out.println("Top element: " + s.peek());
+        // Push 元素
+        for (int i = 1; i <= 5; i++) {
+            stack.push(i);
+            System.out.println("Push: " + i);
+        }
 
-        System.out.println("Pop: " + s.pop());
-        System.out.println("Pop: " + s.pop());
+        // Pop 元素
+        while (!stack.isEmpty()) {
+            System.out.println("Pop: " + stack.pop());
+        }
 
-        System.out.println("Is Empty: " + s.isEmpty());
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+
+        System.out.println("Execution Time (ns): " + duration);
     }
 }
