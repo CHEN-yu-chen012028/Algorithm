b7b28e3b7a901de587ab985f744934ad0f00ce64
陳羽偵 <d13405011@ems.npu.edu.tw>
2026-03-18 16:44:54 +0800

    Implement user input for queue operations in QueueInputExample
    Added a new class QueueInputExample that allows user input for queue operations.

--- QueueExample.java
+++ QueueExample.java
@@ -1,26 +1,35 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
-public class QueueExample {
+public class QueueInputExample {
     public static void main(String[] args) {
-        long startTime = System.nanoTime();
-
+        Scanner scanner = new Scanner(System.in);
         Queue<Integer> queue = new LinkedList<>();
 
-        // Enqueue
-        for (int i = 1; i <= 5; i++) {
-            queue.offer(i);
-            System.out.println("Enqueue: " + i);
+        System.out.print("請輸入數字個數: ");
+        int n = scanner.nextInt();
+
+        long startTime = System.nanoTime();
+
+        // 使用者輸入並 enqueue
+        for (int i = 0; i < n; i++) {
+            System.out.print("輸入數字: ");
+            int num = scanner.nextInt();
+            queue.offer(num);
         }
 
-        // Dequeue
+        System.out.println("\nQueue 輸出 (FIFO):");
+
+        // dequeue 並輸出
         while (!queue.isEmpty()) {
-            System.out.println("Dequeue: " + queue.poll());
+            System.out.println(queue.poll());
         }
 
         long endTime = System.nanoTime();
-        long duration = endTime - startTime;
 
-        System.out.println("Execution Time (ns): " + duration);
+        System.out.println("Execution Time (ns): " + (endTime - startTime));
+
+        scanner.close();
     }
 }
